f30d070f3ac9f78325ae81a0cc1dab9a4738c643
KevinGalvisSA <156678719+KevinGalvisSA@users.noreply.github.com>
2024-05-15 19:50:26 +0000

    feat: :sparkles: Eliminacion delos elementos de reproductor y culminacion de la notificacion

--- src/my-element.js
+++ src/my-element.js
@@ -1,342 +1,139 @@
-import  {LitElement, html, css} from "lit";
-class Myelement extends LitElement{
-  static properties = {
-    titule: {type: String}
-  }
-  constructor(){
+import { LitElement, css, html } from 'lit'
+
+
+export class MyElement extends LitElement {
+  
+
+  constructor() {
     super();
-    this.titule = "Now Playing"
+    this.image = "/src/assets/image20.svg"
+    this.title = "Money Machine"
+    this.description = "1000 Gecks"
+    this.leftArrow= "/src/assets/leftArrow.png"
+    this.playButton = "/src/assets/playbutton.svg"
+    this.rightArrow= "/src/assets/rightArrow.png"
+  } 
 
-    this.song = "Villians and Heroes"
-    this.album = "heros and villians"
-
-    this.plustime = "2:14"
-    this.negativetime = " -1:15"
-
-    this.imagen = "/src/assets/metroboom.png"
-
-    this.dispositive = "Airpos Pro (Dave)"
-  }
-//CSS DEL OSOCOCOCOCOCOCOSOSOSOSOSOSOSOSOSOSOS
-  static styles = css`
-
-  * {
-    margin: 0;
-    padding: 0
-  }
-
-  my-element{
-    background: green;
-    width: 100dvw
-  }
+  static styles =css`
 
 
-  .container{
-    
-    display: grid;
-    width:100% !important;
-    height:100vh;
-    background: white;
-    grid-template-columns: 1fr;
-    grid-template-rows: 0.3fr 3fr 0.5fr 0.5fr 0.5fr 0.5fr 0.3fr;   
-    grid-template-areas: 
-    "titule"
-    "container__image"
-    "song_name"
-    "line_song"
-    "change_song"
-    "song_volumen"
-    "dispositive"
-  
-  }
-  
-
-  /*TITULO DE NOW PLAYING ---------------------------------*/ 
-  .titule{
-    grid-area: titule;
-    color: black;
-    // background: purple;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-
-  /*/IMAGEN DEL MEDIO PARA POER CONTROLARLA----------------/*/
-  .container__image{
-    grid-area: container__image;
-    // background: red;
-    display: flex;
-    align-items: center;
-    flex-wrap: nowrap;
-    justify-content: center;
-    height:100%;
-    width:100%;
-
-  }
-  .container__image > img {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 10px;
-    width: 60%;
-  }
-
-
-  /*PARTE DE NOMBRE SONG_NAME----------------------/*/
-  .song_name{
-    grid-area: song_name;
-    color: #1DB954;
-    // background: gray;
-    display:flex;
-    justify-content: space-evenly;
-    flex-direction: row;
-    align-items: center;
-    
-    
-  }
-
-  .song_name .titule_song{
-    
-    color: black;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    flex-direction: center;
-  }
-
-
-  /*/PARTE DE TIEMPO DE LA CANCION------------------------------------/*/
-  .line_song{
-    // background: brown;
-    display:flex;
-    flex-direction: column;
-    color: black
-  
-  }
-  .line_song .time{
-    display:flex;
-    justify-content: space-between;
-    color: gray
-  }
-
-  .line_song .time_line input[type="range"] {
-    background-color: gray; /* Color de fondo del input */
-    height: 5px; /* Altura del input */
-    border-radius: 2px; /* Redondeamos los bordes */
-    accent-color: #1DB954; /* Color del thumb (verde) */
-
-  }
-
-  .line_song .time_line input[type="range"]::-webkit-slider-thumb {
-    -webkit-appearance: none; /* Quitamos la apariencia por defecto del thumb */
-    width: 20px; /* Ancho del thumb */
-    height: 20px; /* Alto del thumb */
-    border-radius: 50%; /* Redondeamos el thumb */
-    background-color: #00ff00; /* Color del thumb (verde) */
-    cursor: pointer; /* Cambiamos el cursor a una mano */
-  }
-  .line_song input{
-    width: 90%
-
-  }
-  .line_song .time_line{
-    display:flex;
-    justify-content: center
-  }
-
-
-/*  /PARTE DE CAMBIAR LA CANCION CHANGE_SONG----------------------/*/
-  .change_song{
-    grid-area: change_song;
-    // background: orange;
-    display:flex;
-    justify-content: space-evenly;
-
-  }
-  .change_song img{
-
-
-    background-size: contain;
-    width: 1.5vw; /* ajusta el ancho y alto según sea necesario */
-    height: 100%;
-    border: none;
-    cursor: pointer; /* cambia el cursor a una mano para indicar que es clickable */
-
-  }
-  .change_song .play{
-
-    background-size: contain;
-    width: 6vw; /* ajusta el ancho y alto según sea necesario */
-    height: 100%;
-    border: none;
-    cursor: pointer; /* cambia el cursor a una mano para indicar que es clickable */
-
-  }
-
-
-  /*PARTE DE VOLUMEN DEL DISPOSITIVO-----------------------------/ */
-  .song_volumen{
-    grid-area: song_volumen;
-    // background: blue;
-    display:flex;
-    justify-content: center;
-  
-  }
-  .song_volumen .volumen_line input[type="range"] {
-    background-color: gray; /* Color de fondo del input */
-    height: 5px; /* Altura del input */
-    width:50%;
-    border-radius: 2px; /* Redondeamos los bordes */
-    accent-color: #1DB954; /* Color del thumb (verde) */
-
-  }
-
-  .song_volumen .volumen_line input[type="range"]::-webkit-slider-thumb {
-    -webkit-appearance: none; /* Quitamos la apariencia por defecto del thumb */
-    width: 20px; /* Ancho del thumb */
-    height: 20px; /* Alto del thumb */
-    border-radius: 50%; /* Redondeamos el thumb */
-    background-color: #00ff00; /* Color del thumb (verde) */
-    cursor: pointer; /* Cambiamos el cursor a una mano */
-  }
-  .volumen_line{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    width:100%
-
-  }
-  .volumen_line input{
-    width: 80%
-
-  }
-  .song_volumen .aleatory{
-    background-size: contain;
-    width: 1.5vw; /* ajusta el ancho y alto según sea necesario */
-    height: 100%;
-    border: none;
-    cursor: pointer; /* cambia el cursor a una mano para indicar que es clickable */
-  }
-
-
-  /*DISPOSITIVE--------------------------------------------------*/
-  .dispositive{
-    grid-area: dispositive;
-    display: flex;
-    align-items: center;
-    align-content: center;
-    justify-content: center;
-    
-  }
-  .info_dispositive {
-    display: flex;
-    align-items: center;
-    background: #1db95434;
-    border-radius: 20px;
-    width: 10vw;
-    color: gray;
-    align-content: stretch;
-    justify-content: space-around;
-    
-  }
-  @media screen and (max-width: 768px){
-  /*  /PARTE DE CAMBIAR LA CANCION CHANGE_SONG----------------------/*/
-    .change_song{
-      grid-area: change_song;
-      /*background: orange;*/
-      display:flex;
-      justify-content: space-evenly;
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
     }
-    .change_song .play{
-      width: 20vw;
-      height: 100%;
-      border: none;
-      cursor: pointer;
-    }
-    .change_song img{
-      width: 6%; /* ajusta el ancho y alto según sea necesario */
-      height: 100%;
-      border: none;
-      cursor: pointer; /* cambia el cursor a una mano para indicar que es clickable */
-    }
-    /*DISPOSITIVE--------------------------------------------------*/
-    .dispositive{
-      grid-area: dispositive;
+
+    .container {
       display: flex;
       align-items: center;
-      align-content: center;
+      flex-direction: column;
       justify-content: center;
-      
+      width: 250px;
+      height: 350px;
+      border-radius: 20px;
+      position: relative;
+      overflow: hidden;
     }
-    .info_dispositive {
+
+    .container::before{
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-image: url(/src/assets/image20.svg);
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      filter: blur(10px);
+      z-index: -1;
+    }
+
+    .atras img{
+      width: 100%;
+      height: 100%;
+    }
+
+    .atras{
       display: flex;
       align-items: center;
-      background: #1db95434;
-      border-radius: 20px;
-      width: 50%;
-      color: gray;
-      align-content: stretch;
-      justify-content: space-around;
-      
+      justify-content: center;
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      top: 40px;
+      left: 10px;
+      z-index: 2;
     }
-  }`
-  //HTML DEL COSOSOSOSOSOSOSOOCODOCOSOCSOCSOCSOSOSOS
+
+    .main_container{
+      color: #fff;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex-direction: column;
+      gap: 5px;
+      position: relative;
+      z-index: 1;
+    }
+
+    .main_container .bck_img{
+      display: flex;
+    }
+
+    .bck_img img{
+      width: 200px;
+      max-height: 250px;
+    }
+
+    .interactive_buttons{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    button{
+      border-radius: 100px;
+      border-style: none;
+      background-color: transparent;
+    }
+
+    .button_play{
+      display: flex;
+      justify-content: center;
+      align-items: center;  
+      margin: 10px;
+      border-radius: 100%;
+      font-size: 50px;
+      width: 40px;
+      height: 40px;
+    }
+  `
   render(){
     return html`
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <div class="container">
-      <div class="titule">
-      <h3>${this.titule}<h3>
-    </div>
-    <div class="container__image">
-      <img src=${this.imagen}>
-    </div>
-    <div  class="song_name">
-      <div class="plus">
-        <i class='bx bx-plus'></i>
+      <div class="container">
+
+        <div class="atras"><img src="/src/assets/atras.svg"></div>
+        
+        <div class="main_container"> 
+          <div class="bck_img">
+          <img src="${this.image}">
+          </div>
+          <h2>${this.title}</h2>
+          <h3>${this.description}</h3>
+        </div>
+
+
+        <div class= "interactive_buttons">
+          <button><img src="${this.leftArrow}"></button> 
+          <button class= "button_play"><img src="${this.playButton}"></button> 
+          <button><img src="${this.rightArrow}"></button>
+        </div>
       </div>
-      <div class="titule_song">
-        <h2>${this.song}</h2>
-        <p>${this.album}</p>
-      </div>
-      <div class="like">
-        <i class='bx bx-heart' ></i>
-      </div>
-    </div>
-    <div class="line_song">
-      <div class="time">
-        <p>${this.plustime}</p>
-        <p>${this.negativetime}</p>
-      </div>
-      <div class="time_line">
-        <input type="range" name="slider" id="slide2">
-      </div>
-    </div>
-    <div class="change_song">
-      <img src="src/assets/Group 45.svg" class="aleatory">
-      <img src="src/assets/Group.svg"  class="left">
-      <img src="src/assets/play button.svg" class="play">
-      <img src="src/assets/Group right.svg" class="right">
-      <img src="src/assets/Group 44.svg" class="rewind">
-    </div>
-    <div class="song_volumen">
-      <div class="volumen_line">
-        <img src="src/assets/low_sound.svg" class="aleatory">
-        <input type="range" name="slider" id="slide2">
-        <img src="src/assets/up_sound.svg" class="aleatory">
-      </div>
-    </div>
-    <div class="dispositive">
-      <div class="info_dispositive">
-        <img src="src/assets/Vector.svg" class="aleatory">
-        <p>${this.dispositive}</p>
-      </div>
-    </div>
-    `
+    `;
   }
-  _mostrar(){
-    this.name = e.target.value
-  }
+
 }
-customElements.define("my-element", Myelement)
+
+window.customElements.define('my-element', MyElement)
